c7bca5bedc7cf9171ac23df4555885757313caab
sinxnrah <sinxnrahman8d@gmail.com>
2026-06-30 12:42:12 -0400

    Update 07 solution to improve efficiency

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,9 +1,5 @@
 const reverseString = function(str) {
-    let reversed = [];
-    for (let i = str.length - 1; i >= 0; i--) {
-        reversed.push(str[i]);
-    }
-    return reversed.join('');
+    return str.split("").reverse().join("");
 };
 
 // Do not edit below this line
