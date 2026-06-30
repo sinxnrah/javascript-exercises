22aa57ff70eb3c5dc8dc8e9260e28b708eaf2ca7
sinxnrah <sinxnrahman8d@gmail.com>
2026-06-30 11:19:05 -0400

    Complete 07 - reverse string

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reversed = [];
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversed.push(str[i]);
+    }
+    return reversed.join('');
 };
 
 // Do not edit below this line
