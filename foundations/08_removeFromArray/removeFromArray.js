3b024230456ddbc307dca415f76e6d30bf76d872
sinxnrah <sinxnrahman8d@gmail.com>
2026-07-06 22:53:23 -0400

    Complete 08 removeFromArray

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    
+    return array.filter((item) => !args.includes(item));
 };
+
+removeFromArray([1, 2, 2, 3], 2)
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
