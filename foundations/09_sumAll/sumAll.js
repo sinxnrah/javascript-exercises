9f27f60a366b96ece29345027755c04f7e1da346
sinxnrah <sinxnrahman8d@gmail.com>
2026-07-07 00:28:06 -0400

    Refactor 09 sumAll to use O(1) Gauss formula

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,17 +1,13 @@
 const sumAll = function(num1, num2) {
 
-    if (num1 < 0 || num2 < 0) return "ERROR";
     if (!Number.isInteger(num1) || !Number.isInteger(num2)) return "ERROR";
+    if (num1 < 0 || num2 < 0) return "ERROR";
 
-    let sum = 0;
-    const start = Math.min(num1, num2);
-    const end = Math.max(num1, num2);
+    const min = Math.min(num1, num2);
+    const max = Math.max(num1, num2);
+    const count = max - min + 1;
 
-    for (let i = start; i <= end; i++) {
-        sum += i;    
-    }
-
-    return sum;
+    return count * (max + min) / 2;
 };
 
 sumAll(1,4);
