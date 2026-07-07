6d7a302cba8a92efb2a5b0bbb0d141225aeb39e5
sinxnrah <sinxnrahman8d@gmail.com>
2026-07-07 09:47:22 -0400

    Complete 11 tempConversion

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,12 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degF) {
+  let degC = (degF - 32) * (5 / 9);
+  return Math.round(degC * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degC) {
+  let degF = degC * (9 / 5) + 32;
+  return Math.round(degF * 10) / 10;
+  
 };
 
 // Do not edit below this line
