61e4cc9c15ed74da54a0747a7e68b05cba10366b
sinxnrah <sinxnrahman8d@gmail.com>
2026-07-22 20:04:40 -0400

    Completed several exercises

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.reduce((oldest, current) => {
+        if (
+            (current.yearOfDeath || new Date().getFullYear()) - current.yearOfBirth >
+            (oldest.yearOfDeath || new Date().getFullYear()) - oldest.yearOfBirth
+        ) {
+            oldest = current;
+        }
+        return oldest;
+    });
 };
 
 // Do not edit below this line
--- foundations/16_findTheOldest/solution/findTheOldest-solution.js
+++ foundations/16_findTheOldest/solution/findTheOldest-solution.js
@@ -16,12 +16,7 @@ const findTheOldest = function (people) {
   });
 };
 
-/* ALTERNATIVE SOLUTION
 const getAge = function (person) {
-  // The nullish coalescing assignment operator
-  // only does the assignment if the left side is "nullish" (evaluates to undefined or null)
-  // if the left side has any other value, no assignment happens
-  // here, we use ??= to set the current year for our subtraction below only if there is no year of death
   person.yearOfDeath ??= new Date().getFullYear();
 
   return person.yearOfDeath - person.yearOfBirth;
@@ -35,6 +30,6 @@ const findTheOldest = function (people) {
   const oldestPerson = peopleOldestToYoungest[0];
   return oldestPerson;
 };
-*/
+
 
 module.exports = findTheOldest;
